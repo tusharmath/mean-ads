1ae3b85e5ef39d0f734a5842930f7d176c9f85dc
Tushar Mathur <tusharmath@gmail.com>
2014-08-10 12:17:06 +0530

    Updating the paths for bower installation

--- setup/dev.sh
+++ setup/dev.sh
@@ -1,2 +1,10 @@
 #NPM Global
-npm install -g coffee-script gulp
+npm install -g coffee-script gulp bower
+
+#Local Installations
+npm install
+
+#Bower Installation
+bower install
+
+echo "Start Server: gulp serve"
